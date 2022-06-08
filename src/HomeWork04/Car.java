b0c0775bb61233cf5fdac41cff7958fda1422fed
Slava <_fish_@ukr.net>
2022-06-08 17:11:28 +0300

    08.06.22 hw04

--- src/HomeWork04/Car.java
+++ src/HomeWork04/Car.java
@@ -1,2 +1,25 @@
-package HomeWork04;public class Car {
+package HomeWork04;
+public class Car {
+    public double vol;
+    public double rest;
+    public double cons;
+
+
+    public Car(double vol, double rest, double cons) {
+        this.vol = vol;
+        this.rest = rest;
+        this.cons = cons;
+    }
+
+    public double full(double vol, double rest) {
+        return vol - rest;
+    }
+
+    public double remaining(double rest, double cons, double N) {
+        return rest - ((cons / 100) * N);
+    }
+
+    public double refueling(double rest, double cons, double N) {
+        return (cons / 100) * N - rest;
+    }
 }
--- src/HomeWork04/Main.java
+++ src/HomeWork04/Main.java
@@ -1,2 +1,31 @@
-package HomeWork04;public class Main {
+package HomeWork04;
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(50.0, 20.0, 10.0);
+        double refueling = 0.0;
+        double r = 0.0;
+        double zeroRest = car.rest;
+        double price = 50.0;
+        // Одесса - Кривое Озеро 178,5 км
+        car.rest = car.remaining(car.rest, car.cons, 178.5);
+        refueling = car.refueling(car.rest, car.cons, 156.0);
+        r = refueling;
+        double r1 = r;
+        System.out.println("Остаток топлива в Кривое Озеро: " + car.rest);
+        System.out.println("Дозаправка в Кривое озеро: " + r);
+        // Кривое Озеро - Жашков 156,0 км
+        car.rest = car.remaining(car.rest, car.cons, 156.0) + refueling ;
+        refueling = car.refueling(car.rest, car.cons, 148.2);
+        r = refueling;
+        double r2 = r;
+        System.out.println("Остаток топлив в Жашков: " + car.rest);
+        System.out.println("Дозаправка в Жашков: " + r);
+        // Жашков - Киев 148,2 км
+        car.rest = car.remaining(car.rest, car.cons, 148.2) + refueling;
+        System.out.println("Остаток топлив в Киев: " + car.rest);
+        System.out.println();
+        // Одесса - Киев 476,0 км
+        System.out.println("Итого дозаправлено: " + (r1+r2) + "  " + "за " + ((r1+r2)*price) + " грн.");
+        System.out.println("Стоимость всей поездки: " + (zeroRest+(r1+r2))*price + " грн.");
+    }
 }
