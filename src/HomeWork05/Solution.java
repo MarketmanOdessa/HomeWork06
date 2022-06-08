8fce947011edc8e19aec22ca9b27730b7a3378b4
Slava <_fish_@ukr.net>
2022-06-08 17:18:23 +0300

    08.06.22 hw05

--- src/HomeWork05/Solution.java
+++ src/HomeWork05/Solution.java
@@ -1,2 +1,102 @@
-package HomeWork05;public class Solution {
+package HomeWork05;
+
+import java.util.Arrays;
+
+import static HomeWork05.CreateStudents.getStudentArray;
+
+public class Solution {
+    public static void main(String[] args) {
+        faculty(getStudentArray(), "Химия");
+        studentFacultyAndCours(getStudentArray());
+        year(getStudentArray(), 1997);
+        listGroup(getStudentArray(), "Х20");
+    }
+
+
+    static void faculty(Student[] students, String faculty) {
+        System.out.println("Студенты на факультете " + faculty + ":");
+        for (Student student : students) {
+            if (student.getFaculty().equals(faculty)) {
+                System.out.println(student.toString());
+            }
+        }
+        System.out.println();
+    }
+
+    static void studentFacultyAndCours(Student[] students) {
+        System.out.println("Список студенитов по курсам");
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1 + "курс:");
+            for (Student student : students) {
+                if (student.getCourse() == (i + 1))
+                    System.out.println(student.toString());
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        String[] faculty = new String[students.length];
+        int k = 0;
+        faculty[k] = students[0].getFaculty();
+        k++;
+
+        for (Student student : students) {
+            boolean fl = true;
+            for (String s : faculty) {
+                if (student.getFaculty().equals(s)) {
+                    fl = false;
+                    break;
+                }
+            }
+
+            if (fl) {
+                faculty[k] = student.getFaculty();
+                k++;
+            }
+        }
+
+        if (k < students.length) {
+            String[] mass = new String[k];
+            for (int i = k; i < faculty.length; i++) {
+                faculty[i] = "";
+            }
+
+            System.arraycopy(faculty, 0, mass, 0, k);
+            faculty = mass;
+        }
+
+        Arrays.sort(faculty);
+
+
+        for (String s : faculty) {
+            System.out.println("Факультет " + s + ":");
+            for (Student student : students) {
+                if (student.getFaculty().equals(s)) {
+                    System.out.println(student.toString());
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void year(Student[] students, int year) {
+        System.out.println("Список студентов, родившихся после " + year + ":");
+        for (Student student : students) {
+            if (student.getYearOfBirth() > year) {
+                System.out.println(student.toString());
+            }
+        }
+        System.out.println();
+    }
+
+    static void listGroup(Student[] students, String group) {
+        System.out.println("Cписок учебной группы " + group + ":");
+        for (Student student : students) {
+            if (student.getGroup().equals(group)) {
+                System.out.println(student.toString());
+            }
+        }
+    }
 }
--- src/HomeWork05/Student.java
+++ src/HomeWork05/Student.java
@@ -1,2 +1,140 @@
-package HomeWork05;public class Student {
+package HomeWork05;
+
+import java.util.Objects;
+
+public class Student {
+    private int id;
+    private String Surname;
+    private String FirstName;
+    private String Patronymic;
+    private int YearOfBirth;
+    private String Address;
+    private String PhoneNumber;
+    private String Faculty;
+    private int Course;
+    private String Group;
+
+
+
+    public Student(int id, String surname, String firstName, String patronymic, int yearOfBirth, String address, String phoneNumber, String faculty, int course, String group) {
+        this.id = id;
+        Surname = surname;
+        FirstName = firstName;
+        Patronymic = patronymic;
+        YearOfBirth = yearOfBirth;
+        Address = address;
+        PhoneNumber = phoneNumber;
+        Faculty = faculty;
+        Course = course;
+        Group = group;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getPatronymic() {
+        return Patronymic;
+    }
+
+    public int getYearOfBirth() {
+        return YearOfBirth;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public String getFaculty() {
+        return Faculty;
+    }
+
+    public int getCourse() {
+        return Course;
+    }
+
+    public String getGroup() {
+        return Group;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public void setPatronymic(String patronymic) {
+        Patronymic = patronymic;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        YearOfBirth = yearOfBirth;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public void setFaculty(String faculty) {
+        Faculty = faculty;
+    }
+
+    public void setCourse(int course) {
+        Course = course;
+    }
+
+    public void setGroup(String group) {
+        Group = group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && YearOfBirth == student.YearOfBirth && Course == student.Course && Objects.equals(Surname, student.Surname) && Objects.equals(FirstName, student.FirstName) && Objects.equals(Patronymic, student.Patronymic) && Objects.equals(Address, student.Address) && Objects.equals(PhoneNumber, student.PhoneNumber) && Objects.equals(Faculty, student.Faculty) && Objects.equals(Group, student.Group);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Surname, FirstName, Patronymic, YearOfBirth, Address, PhoneNumber, Faculty, Course, Group);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", Surname='" + Surname + '\'' +
+                ", FirstName='" + FirstName + '\'' +
+                ", Patronymic='" + Patronymic + '\'' +
+                ", YearOfBirth=" + YearOfBirth +
+                ", Address='" + Address + '\'' +
+                ", PhoneNumber='" + PhoneNumber + '\'' +
+                ", Faculty='" + Faculty + '\'' +
+                ", Course='" + Course + '\'' +
+                ", Group='" + Group + '\'' +
+                '}';
+    }
 }
