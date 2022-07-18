b5171fe3b008a9649384e1d863f9e39d9ea8865b
Slava <_fish_@ukr.net>
2022-07-18 18:34:53 +0300

    18.07.22 hw12

--- src/test/java/HomeWork07/SimpleCollectionTest.java
+++ src/test/java/HomeWork07/SimpleCollectionTest.java
@@ -1,30 +1,42 @@
 package HomeWork07;
 
-
-import org.junit.jupiter.api.Assertions;
-
 class SimpleCollectionTest {
 
     @org.junit.jupiter.api.Test
     void add() {
         SimpleCollection sc = new SimpleCollection();
-        boolean b = sc.add(1,"1");
-        Assertions.assertTrue(equals(true,b));
+        sc.add(1,"1");
+        assert sc.get(1).equals("1");
     }
 
     @org.junit.jupiter.api.Test
     void testAdd() {
+        SimpleCollection sc = new SimpleCollection();
+        sc.add("Zero");
+        sc.add("One");
+        sc.add("Two");
+        assert sc.get(7).equals("Two");
     }
 
     @org.junit.jupiter.api.Test
     void delete() {
+        SimpleCollection sc = new SimpleCollection();
+        sc.delete(1);
+        assert sc.get(1) == null;
     }
 
     @org.junit.jupiter.api.Test
     void testDelete() {
+        SimpleCollection sc = new SimpleCollection();
+        sc.delete("2");
+        assert sc.get(1) == null;
     }
 
     @org.junit.jupiter.api.Test
     void get() {
+        SimpleCollection sc = new SimpleCollection();
+        sc.add("Zero");
+        String s = sc.get(5);
+       assert s.equals("Zero");
     }
 }
